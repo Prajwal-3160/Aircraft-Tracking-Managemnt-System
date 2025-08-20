05e975bf46622a7405121e6828ef7994d8fd897c
Prajwal Markad <prajwalmarkad7@gmail.com>
2025-08-20 11:31:05 +0530

    Create AircraftApplication.java

---
+++ Backend/com.example.aircraft/AircraftApplication.java
@@ -0,0 +1,12 @@
+package com.example.aircraft;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftApplication.class, args);
+        System.out.println("...Aircraft Started...");
+    }
+}
+
