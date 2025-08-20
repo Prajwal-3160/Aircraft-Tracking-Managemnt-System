c0a0acfaca6bfa94a70959d1245fe78a68027bcf
Prajwal Markad <prajwalmarkad7@gmail.com>
2025-08-20 22:16:57 +0530

    Create AdminDashboardRepository.java

---
+++ Backend/com.example.aircraft.repository/AdminDashboardRepository.java
@@ -0,0 +1,7 @@
+package com.example.aircraft.repository;
+
+import com.example.aircraft.model.AdminDashboard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminDashboardRepository extends JpaRepository<AdminDashboard, Long> {
+}
